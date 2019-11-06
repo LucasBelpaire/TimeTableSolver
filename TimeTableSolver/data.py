54e9c3f98061267c7a7fcbb82bd16acee347d97b
Lucas Belpaire <lucas.belpaire@ugent.be>
2019-11-06 10:07:23 +0100

    fixed typo

--- TimeTableSolver/data.py
+++ TimeTableSolver/data.py
@@ -1,12 +1,12 @@
 class GeneralInfo:
     def __init__(self, academy_year, semester, kilometer_penalty,
-                 late_hours_penalty, not_home_penalty, mini_amount_students):
+                 late_hours_penalty, not_home_penalty, min_amount_students):
         self.academy_year = academy_year
         self.semester = semester
         self.kilometer_penalty = kilometer_penalty
         self.late_hour_penalty = late_hours_penalty
         self.not_home_penalty = not_home_penalty
-        self.mini_amount_students = mini_amount_students
+        self.min_amount_students = min_amount_students
         self.course_hours = {1: "08u30-10u00",
                              2: "10u00-11u30",
                              3: "11u30-13u00",
@@ -24,6 +24,7 @@ class Course:
         self.name = name
         self.student_amount = student_amount
         self.contact_hours = contact_hours
+        self.real_hours = contact_hours*0.8
         self.lecturers = lecturers
         self.curricula = curricula
 
