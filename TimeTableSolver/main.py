dcde4ac213699d3ea9da9ab9dcb327776a0f0312
Lucas Belpaire <lucas.belpaire@ugent.be>
2019-12-04 17:55:14 +0100

    output for 1 type

--- TimeTableSolver/main.py
+++ TimeTableSolver/main.py
@@ -2,9 +2,6 @@ import construct_timetable as ctt
 import process_input
 import general_info as gi
 import generate_output as go
-import global_variables as gv
-import process_input as pi
-import copy
 import time
 import feasible_timetable
 
@@ -15,7 +12,13 @@ def main():
     print("Starting to process input.  " + str(time.perf_counter() - start_time))
     process_input.init_general_info()
     timetable = process_input.create_initial_timetable()
-    events_1, events_2 = process_input.create_initial_events_lists()
+    events_1, events_2, events_3, events_4 = process_input.create_initial_events_lists()
+
+    print(len(events_1))
+    print(len(events_2))
+    print(len(events_3))
+    print(len(events_4))
+
     courses_set = gi.courses_set
     print("Processing input completed.  " + str(time.perf_counter() - start_time))
 
@@ -29,12 +32,12 @@ def main():
 
     # Start the second phase of the feasibility process: tabu search
     print("Starting tabu search phase. " + str(time.perf_counter() - start_time))
-    feasible_timetable.tabu_search()
+    # feasible_timetable.tabu_search()
     print("Completed tabu search phase.  " + str(time.perf_counter() - start_time))
 
     # Start generating the output file
     print("Starting to generate output.  " + str(time.perf_counter() - start_time))
-    go.generate_output_from_time_table()
+    go.generate_output_from_time_table(timetable.timetable)
     print("Generating output completed.  " + str(time.perf_counter() - start_time))
 
 
