c80e1285516a0e8bcc3f56143498a33ffe7c8d2c
Lucas Belpaire <lucas.belpaire@ugent.be>
2019-12-04 17:37:28 +0100

    refactoring

--- TimeTableSolver/feasible_timetable.py
+++ TimeTableSolver/feasible_timetable.py
@@ -1,6 +1,6 @@
 import neighborhood
-import global_variables as gv
 import hard_constraints as hc
+import general_info as gi
 import data
 import time
 import random
@@ -24,9 +24,9 @@ def position_swap(tabu_list):
     tabu_list.append((position_2, position_1))
 
     # make a back up, so a rollback is possible
-    events_back_up = copy.copy(gv.events)
-    empty_positions_back_up = copy.copy(gv.empty_positions)
-    time_table_back_up = copy.copy(gv.time_table)
+    events_back_up = copy.copy(gi.events)
+    empty_positions_back_up = copy.copy(gi.empty_positions)
+    time_table_back_up = copy.copy(gi.time_table)
 
     success = neighborhood.swap_positions(position_1, position_2, feasibility=False)
 
@@ -34,39 +34,39 @@ def position_swap(tabu_list):
         return False
 
     # shuffle events, and try to place them in a random order
-    random.shuffle(gv.events)
-    events_copy = copy.copy(gv.events)
+    random.shuffle(gi.events)
+    events_copy = copy.copy(gi.events)
     for event in events_copy:
-        for position in gv.empty_positions:
+        for position in gi.empty_positions:
             room_fi_number = position[0]
             time_slot = position[1]
-            room = gv.class_rooms_dict[room_fi_number]
+            room = gi.class_rooms_dict[room_fi_number]
             if hc.course_event_fits_into_time_slot(event, time_slot) and hc.room_capacity_constraint(event, room):
-                gv.assign_course_to_position(event, position)
+                gi.assign_course_to_position(event, position)
                 break
 
-    distance = len(gv.events)
+    distance = len(gi.events)
     delta_e = distance - last_distance
 
     if delta_e > 0:
-        gv.events = events_back_up
-        gv.empty_positions = empty_positions_back_up
-        gv.time_table = time_table_back_up
+        gi.events = events_back_up
+        gi.empty_positions = empty_positions_back_up
+        gi.time_table = time_table_back_up
         return False
 
     # Success!
     last_distance = distance
     if distance < best_distance:
-        best_feasible_tt = copy.deepcopy(gv.time_table)
+        best_feasible_tt = copy.deepcopy(gi.time_table)
         best_distance = distance
     return True
 
 
 def split_event(tabu_list):
     # sort events by largest student amount
-    gv.events.sort(key=lambda ev: ev.student_amount, reverse=True)
+    gi.events.sort(key=lambda ev: ev.student_amount, reverse=True)
     # get the course with the most amount of students
-    event = gv.events.pop(0)
+    event = gi.events.pop(0)
     # check if this event is not in the tabu list
     if event in tabu_list:
         return False
@@ -77,7 +77,7 @@ def split_event(tabu_list):
     student_amount_2 = event.student_amount - student_amount_1
     curricula = event.curricula
     event_number = event.event_number
-    course = gv.courses_dict[course_code]
+    course = gi.courses_dict[course_code]
     course.course_hours += 1  # because the event is split into two, an extra course hour should be created
     event_1 = data.CourseEvent(course_code=course_code,
                                lecturers=lecturers,
@@ -92,19 +92,19 @@ def split_event(tabu_list):
     # add the new events to the tabu list
     tabu_list.append(event_1)
     tabu_list.append(event_2)
-    gv.events.append(event_1)
-    gv.events.append(event_2)
-    random.shuffle(gv.events)
+    gi.events.append(event_1)
+    gi.events.append(event_2)
+    random.shuffle(gi.events)
 
     # check if it is possible to place extra events
-    events_copy = copy.copy(gv.events)
+    events_copy = copy.copy(gi.events)
     for event in events_copy:
-        for position in gv.empty_positions:
+        for position in gi.empty_positions:
             room_fi_number = position[0]
             time_slot = position[1]
-            room = gv.class_rooms_dict[room_fi_number]
+            room = gi.class_rooms_dict[room_fi_number]
             if hc.course_event_fits_into_time_slot(event, time_slot) and hc.room_capacity_constraint(event, room):
-                gv.assign_course_to_position(event, position)
+                gi.assign_course_to_position(event, position)
                 break
 
     return
@@ -119,14 +119,14 @@ def tabu_search():
     tabu_split = []
 
     # Add all unplaced events to events
-    gv.events += gv.unplaced_events
+    gi.events += gi.unplaced_events
 
-    best_distance = len(gv.events)
+    best_distance = len(gi.events)
     print(best_distance)
     last_distance = best_distance
-    best_feasible_tt = copy.deepcopy(gv.time_table)
+    best_feasible_tt = copy.deepcopy(gi.time_table)
 
-    while len(gv.events) > 0 and time.clock() < starting_time + max_time:
+    while len(gi.events) > 0 and time.clock() < starting_time + max_time:
         # if tabu list is full, remove the oldest entry
         if len(tabu_positions) == tabu_length:
             tabu_positions.pop(0)
@@ -139,5 +139,5 @@ def tabu_search():
             position_swap(tabu_positions)
         if action == 100:
             split_event(tabu_split)
-    gv.time_table = best_feasible_tt
+    gi.time_table = best_feasible_tt
     return best_distance, best_feasible_tt
